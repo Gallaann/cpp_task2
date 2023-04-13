45988b3ea804b4481691df82746b7e6dedc67491
Gallaann <reindzer26rus@gmail.com>
2023-04-13 16:33:37 +0300

    added cat operation

---
+++ include/CatOperation.h
@@ -0,0 +1,49 @@
+//
+// Created by gallaann on 12.04.23.
+//
+
+#pragma once
+
+#include "IOperation.h"
+
+#include <fstream>
+#include <iostream>
+
+class CatOperation : public IOperation {
+public:
+    void ProcessLine(const std::string &str) override {
+        m_nextOp->ProcessLine(str);
+    }
+
+    void HandleEndOfInput() override {
+        std::ifstream input(m_fileName);
+
+        if (!input) {
+            std::cerr << "Error: could not open file: " << m_fileName << std::endl;
+            return;
+        }
+
+        std::string currentLine;
+        while (getline(input, currentLine)) {
+            if (m_nextOp) {
+                m_nextOp->ProcessLine(currentLine);
+            }
+            std::cout << currentLine << std::endl;
+        }
+
+        if (m_nextOp) {
+            m_nextOp->HandleEndOfInput();
+            return;
+        }
+    }
+
+    void SetNextOperation(IOperation *nextOp) override {
+        m_nextOp = nextOp;
+    }
+
+    CatOperation(const std::string &fileName) : m_fileName(fileName), m_nextOp(nullptr) {}
+
+private:
+    std::string m_fileName;
+    IOperation *m_nextOp;
+};
