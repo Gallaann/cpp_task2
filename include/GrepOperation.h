a0837e2b5c3b9d0d5fd1d27c2cf443ef0457c1b8
Gallaann <reindzer26rus@gmail.com>
2023-04-13 16:33:59 +0300

    added grep operation

---
+++ include/GrepOperation.h
@@ -0,0 +1,37 @@
+//
+// Created by gallaann on 12.04.23.
+//
+
+#include "IOperation.h"
+
+#include <iostream>
+
+class GrepOperation : public IOperation {
+public:
+    void ProcessLine(const std::string &str) override {
+        if (str.find(m_subString) != std::string::npos) {
+            if (m_nextOp) {
+                m_nextOp->ProcessLine(m_subString);
+                return;
+            }
+            std::cout << str;
+        }
+    }
+
+    void HandleEndOfInput() override {
+        if (m_nextOp) {
+            m_nextOp->HandleEndOfInput();
+            return;
+        }
+    }
+
+    void SetNextOperation(IOperation *nextOp) override {
+        m_nextOp = nextOp;
+    }
+
+    GrepOperation(const std::string &subString) : m_subString(subString), m_nextOp(nullptr) {}
+
+private:
+    std::string m_subString;
+    IOperation *m_nextOp;
+};
